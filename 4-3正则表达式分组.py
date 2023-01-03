5a98b067858ce6ece0a1a31b9ca47d2d79b84ddf
lvzhenchao <1370164516@qq.com>
2023-01-03 15:37:01 +0800

    Update 4-3正则表达式分组.py

--- 4-3正则表达式分组.py
+++ 4-3正则表达式分组.py
@@ -50,18 +50,18 @@ print(pattern_3.findall(website))
 html="""
     <div class="movie-item-info">
         <p class="name">
-        <a title="你好，李焕英">你好，李焕英</a>
+            <a title="你好，李焕英">你好，李焕英</a>
         </p>
         <p class="star">
-        主演：贾玲,张小斐,沈腾
+            主演：贾玲,张小斐,沈腾
         </p>    
     </div>
     <div class="movie-item-info">
         <p class="name">
-        <a title="刺杀，小说家">刺杀，小说家</a>
+            <a title="刺杀，小说家">刺杀，小说家</a>
         </p>
         <p class="star">
-        主演：雷佳音,杨幂,董子健,于和伟
+            主演：雷佳音,杨幂,董子健,于和伟
         </p>    
     </div> 
 """
