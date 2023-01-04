b15f61700cb04e5756e7ba6bc9ad2f5a7eebc52b
lvzhenchao <1370164516@qq.com>
2023-01-04 17:16:11 +0800

    Update 5-1-csv.py

--- 5-1-csv.py
+++ 5-1-csv.py
@@ -23,3 +23,13 @@ with open('aggs.csv', 'w', newline='') as f:
     writer.writerows([('hello','world'), ('I','love','you')])
 
 # 2> csv.DictWriter() 类似字典的形式读写数据
+with open('names.csv', 'w', newline='') as csvfile:
+    #构建字段名称，也就是key
+    fieldnames = ['first_name', 'last_name']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    # 写入字段名，当做表头
+    writer.writeheader()
+    # 多行写入
+    writer.writerows([{'first_name': 'Baked', 'last_name': 'Beans'},{'first_name': 'Lovely', 'last_name': 'Spam'}])
+    # 单行写入
+    writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
