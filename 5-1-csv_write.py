30d4016a3dbec9022137bd6832f4ab7312d50b6c
lvzhenchao <1370164516@qq.com>
2023-01-09 13:57:33 +0800

    Update 5-1-csv_write.py

--- 5-1-csv_write.py
+++ 5-1-csv_write.py
@@ -12,8 +12,8 @@ import csv
 with open('eggs.csv', 'w', newline='') as csvfile:
     # delimiter 指定分隔符，默认为逗号，这里指定为空格
     # quotechar 表示引用符  quotechar 是引用符，当一段话中出现分隔符的时候，用引用符将这句话括起来，以能排除歧义
-    # writerow 单行写入，列表格式传入数据
     spamwriter = csv.writer(csvfile, delimiter=' ',quotechar='|')
+    # writerow 单行写入，列表格式传入数据
     spamwriter.writerow(['www.biancheng.net'] * 5 + ['how are you'])
     spamwriter.writerow(['hello world', 'web site', 'www.biancheng.net'])
 
