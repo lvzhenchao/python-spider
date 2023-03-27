195a0d12ae29f3529c3ed18b3afad70e9fdff8a0
lvzhenchao <1370164516@qq.com>
2023-03-27 17:40:15 +0800

    Update 9-2-lxml应用.py

--- 9-2-lxml应用.py
+++ 9-2-lxml应用.py
@@ -3,6 +3,44 @@ import requests
 from lxml import etree
 from ua_info import ua_list
 import random
+
+# <dl class="board-wrapper">
+# 	<dd>
+# 		<i class="board-index board-index-1">1</i>
+# 		<a href="/films/1200486" title="我不是药神" class="image-link" data-act="boarditem-click" data-val="{movieId:1200486}">
+# 		  <img src="//s3plus.meituan.net/v1/mss_e2821d7f0cfe4ac1bf9202ecf9590e67/cdn-prod/file:5788b470/image/loading_2.e3d934bf.png" alt="" class="poster-default">
+# 		  <img alt="我不是药神" class="board-img" src="https://p0.pipi.cn/mmdb/d2dad59253751bd236338fa5bd5a27c710413.jpg?imageView2/1/w/160/h/220">
+# 		</a>
+# 		<div class="board-item-main">
+# 			  <div class="board-item-content">
+# 				  <div class="movie-item-info">
+# 					<p class="name"><a href="/films/1200486" title="我不是药神" data-act="boarditem-click" data-val="{movieId:1200486}">我不是药神</a></p>
+# 					<p class="star">
+# 							主演：徐峥,周一围,王传君
+# 					</p>
+# 					<p class="releasetime">上映时间：2018-07-05</p>
+# 				  </div>
+# 				  <div class="movie-item-number score-num">
+# 					<p class="score">
+# 						<i class="integer">9.</i>
+# 						<i class="fraction">6</i>
+# 					</p>
+# 				  </div>
+# 			  </div>
+# 		</div>
+# 	</dd>
+# 	<dd>...</dd>
+# 	<dd>...</dd>
+# 	<dd>...</dd>
+# </dl>
+
+# 1、确定信息元素结构:
+## 所采集的信息【电影名称、主演演员、上映时间】都包含在<dd>标签中，而<dd>标签又包含在<dl>标签中，dl是父辈节点
+# 2、基准表达式：针对每一个dd提取信息，比较繁琐；
+## 将10个<dd>节点放入一个列表，使用for循环遍历每一个节点对象：匹配10个<dd>节点的Xpath表达式称为【基准表达式】
+## xpath_bds='//dl[@class="board-wrapper"]/dd'
+
+
 class MaoyanSpider(object):
     def __init__(self):
         self.url='https://maoyan.com/board/4?offset=50'
