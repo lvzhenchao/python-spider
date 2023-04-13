8b3e45ad4aca566b2b87a280e2596e924a2cc854
lvzhenchao <1370164516@qq.com>
2023-04-13 17:40:24 +0800

    Create 11-1-1多进程补充-进程间通信.py

---
+++ 11-1-1多进程补充-进程间通信.py
@@ -0,0 +1,29 @@
+
+# 每个进程都在自己的独立空间内，互不干扰，但有些情况下想要实现进程间通信也是可以的，我们可以使用Queue、Pipes等工具
+import os
+import random
+import time
+from multiprocessing import Process, Queue
+
+
+def put_data(q):
+    print("Process put_data id:{}".format(os.getpid()))
+    for data in ["云天河", "韩菱纱", "柳梦璃", "慕容紫英"]:
+        print("put data {}".format(data))
+        q.put(data)
+        time.sleep(random.random())
+
+def get_data(q):
+    print("Process get_data id: {}".format(os.getpid()))
+    while True:
+        value = q.get(True)
+        print('Get %s from queue.' % value)
+
+if __name__ == '__main__':
+    queue = Queue()
+    p_put = Process(target=put_data, args=(queue,))
+    p_get = Process(target=get_data, args=(queue,))
+    p_put.start()
+    p_get.start()
+    p_put.join()
+    p_get.terminate()
