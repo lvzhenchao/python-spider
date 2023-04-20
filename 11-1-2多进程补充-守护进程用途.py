65e7108ad744958edc6ad5fa0b001df81321e772
lvzhenchao <1370164516@qq.com>
2023-04-20 15:03:58 +0800

    Update 11-1-2多进程补充-守护进程用途.py

--- 11-1-2多进程补充-守护进程用途.py
+++ 11-1-2多进程补充-守护进程用途.py
@@ -23,6 +23,7 @@ def func(index):
 ## 1、从运行结果看，并没有达到我们的预期，以为父进程和子进程是异步的
 ## 即使加上休眠，也还是异步的
 # 主进程10封邮件已发送完毕
+# 执行时间:0.10
 # 第 4 封邮件发送完毕
 # 第 8 封邮件发送完毕
 # 第 2 封邮件发送完毕
@@ -40,10 +41,10 @@ def func(index):
 #         p = Process(target=func, args=(i,))
 #         p.start()
 #         p.join()
-
+#
 #     print("2:主进程10封邮件已发送完毕")
 #     print('')
-
+#
 #     end = time.time()
 #     #查看程序执行时间
 #     print('执行时间:%.2f' % (end - start))
@@ -61,6 +62,7 @@ def func(index):
 # 第 8 封邮件发送完毕
 # 第 9 封邮件发送完毕
 # 主进程10封邮件已发送完毕
+# 执行时间:5.70
 
 # if __name__ == '__main__':
 #     start = time.time()
@@ -70,7 +72,7 @@ def func(index):
 #         p.start()
 #         p_list.append(p) # 把启动的进程全部添加进去
 #     for p in p_list:
-#         p.join() #添加阻塞
+#         p.join() #给所有的都在进行进程添加阻塞，时间虽然比第一个慢，但是也很快，还是异步的
 #
 #     print("3: 主进程10封邮件已发送完毕")
 #     print('')
@@ -90,22 +92,23 @@ def func(index):
 # 第 2 封邮件发送完毕
 # 第 8 封邮件发送完毕
 # 3: 主进程10封邮件已发送完毕
+# 执行时间:1.26
 
 
 ## 守护进程：是一类在后台运行的特殊进程，是一个在后台运行并且不受任何终端控制的进程；用于执行特定的系统任务
 ## 很多守护进程在系统引导的时候启动，并且一直运行直到系统关闭。另一些只在需要的时候才启动，完成任务后就自动结束。守护进程最重要的特性是后台运行。
-def func0():
-    print('子进程 start')
-    time.sleep(3)
-    print('子进程 end')
-
-if __name__ == '__main__':
-    p = Process(target=func0)
-    # 设置守护进程必须在start()方法之前
-    p.daemon = True #启动守护进程
-    p.start()
-    time.sleep(2)
-    print('主进程')
+# def func0():
+#     print('子进程 start')
+#     time.sleep(3)
+#     print('子进程 end')
+#
+# if __name__ == '__main__':
+#     p = Process(target=func0)
+#     # 设置守护进程必须在start()方法之前
+#     p.daemon = True #启动守护进程
+#     p.start()
+#     time.sleep(2)
+#     print('主进程')
 # 子进程 start
 # 主进程
 # 没有打印：子进程 end
