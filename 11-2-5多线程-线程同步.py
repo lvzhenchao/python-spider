549e2b6d607d37c6425df5556efa1295d64d1aa6
lvzhenchao <1370164516@qq.com>
2023-04-21 17:34:05 +0800

    Update 11-2-5多线程-线程同步.py

--- 11-2-5多线程-线程同步.py
+++ 11-2-5多线程-线程同步.py
@@ -1,44 +1,48 @@
-
-# 一般主线程要等子线程的任务结束之后才会结束，即时调用了exit()
-
-# 可以把子线程设置为守护主线程，这样一旦主线程要结束，子线程也会立即结束
-# 将子线程设置为主线程的方法有三种
-
 import threading
-import time
 
-def run():
-    print('run: 当前的线程为{}'.format(threading.current_thread()))
-    for i in range(5):
-        print('跑：{}'.format(i))
-        time.sleep(1)
+# 如果多个线程同时操作同一个全局变量，可能会造成资源竞争的问题，导致数据出错，解决资源竞争问题的方法有两个
+## 线程同步：调用join()方法，等当前线程结束才会继续执行，但是这样失去了多线程的优势
+## 线程异步：互斥锁，保护共享数据，保证同一时刻只有一个线程可以访问公共资源，谁抢到锁谁就能访问资源，访问完之后释放资源
 
-def eat():
-    print('run：当前的线程为{}'.format(threading.current_thread()))
-    for i in range(5):
-        print('吃饭啦: {}'.format(i))
-        time.sleep(1)
+### 互斥锁3步骤：创建锁、上锁、解锁
+# 创建锁
+# mutex = threading.Lock()
+# 上锁
+# mutex.acquire()
+# 这个部分的代码会被锁定，其他线程不能访问，直到释放锁
+# 释放锁
+# mutex.release()
+
+# 参考：https://binglengdexiwang.blog.csdn.net/article/details/107836757
+
+g_num = 0
+# 第1步：创建锁
+mutex = threading.Lock()
+
+def task1():
+    global g_num
+    # 第2步：上锁
+    mutex.acquire()
+    try:
+        for i in range(1000000):
+            g_num += 1
+    finally:
+        # 第3步：解锁
+        mutex.release()
+    print("task1:",g_num)
+
+def task2():
+    global g_num
+    mutex.acquire()
+    try:
+        for i in range(1000000):
+            g_num += 1
+    finally:
+        mutex.release()
+    print("task2:",g_num)
 
 if __name__ == '__main__':
-    # 1、创建线程
-    run_thread = threading.Thread(target = run)
-    eat_thread = threading.Thread(target = eat)
-    print('main：主线程为', threading.current_thread())
-
-    # 设置为守护主线程
-    ## 方法1创建线程时指定daemon参数
-    # run_thread = threading.Thread(target=run, daemon=True)
-    # eat_thread = threading.Thread(target=eat, daemon=True)
-    # print('main：主线程为', threading.current_thread())
-
-    ## 方法2设置setDaemon()方法:设置为True时，就相当于是守护线程，该线程进入后台工作，可以把其想为不是很重要的线程
-    # run_thread.setDaemon(True)
-    # eat_thread.setDaemon(True)
-
-    ## 方法3设置daemon属性
-    # run_thread.daemon = True
-    # eat_thread.daemon = True
-
-    # 2、开启线程
-    run_thread.start()
-    eat_thread.start()
+    t1 = threading.Thread(target=task1)
+    t2 = threading.Thread(target=task2)
+    t1.start()
+    t2.start()
