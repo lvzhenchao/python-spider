6074c09d5857aecf24927d3fba2c9563383f620a
lvzhenchao <1370164516@qq.com>
2023-07-05 18:22:14 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,7 @@
-def intNum():
-    print("开始执行")
-    for i in range(5):
-        yield i
-        print("继续执行")
-
-# 调用next()内置函数
-# num = intNum()
-# print(next(num))
-# print(next(num))
-# print(next(num))
-# print(next(num))
-# print(num.__next__())
-
-# list() 和 tuple() 底层实现和 for 循环的遍历过程是类似的
-num = intNum()
-print(list(num))
-num = intNum()
-print(tuple(num))
+try:
+    1/0
+except Exception as e:
+    # 访问异常的错误编号和详细信息
+    print(e.args)
+    print(str(e))
+    print(repr(e))
