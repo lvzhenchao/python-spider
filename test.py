8b0a83441fe717cdc8958fc99c210e0213966004
lvzhenchao <1370164516@qq.com>
2023-07-06 15:02:59 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 try:
-    1/0
+    a = input("输入一个数：")
+    #判断用户输入的是否为数字
+    if(not a.isdigit()):
+        raise ValueError("a 必须是数字")
 except Exception as e:
     # 访问异常的错误编号和详细信息
     print(e.args)
