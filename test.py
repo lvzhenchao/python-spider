6a1f9d0eadebf3672ecd9456999baedffe38790d
lvzhenchao <1370164516@qq.com>
2023-07-07 16:22:22 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 import os
 
 print(os.getcwd())
+print(os.path.abspath('.'))
+print(os.path.isabs('.'))
+print(os.path.relpath('E:\python_code\python-spider'))
+print(os.path.relpath('E:\python_code\python-spider', 'E:\python_code'))
+
+path = "E:\python_code\python-spider\demo.txt"
+print(os.path.dirname(path))
